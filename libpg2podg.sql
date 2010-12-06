5d75bf16e6d78b10fcfdf6d0acd3ae61d356d6f3
Gianni Ciolli <gianni.ciolli@2ndQuadrant.it>
2010-12-06 15:34:38 +0000

    tidied up

--- libpg2podg.sql
+++ libpg2podg.sql
@@ -1,3 +1,21 @@
+-- libpg2podg.sql : 2-player Open Deterministic Games
+-- Copyright (C) 2010 Gianni Ciolli <gianni.ciolli@2ndQuadrant.it>
+-- 
+-- This program is free software: you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+------------------------------------------------------------
+
 CREATE TEMPORARY TABLE my_states (
 	t timestamp DEFAULT current_timestamp
 ,	id SERIAL PRIMARY KEY
--- libpgchess.sql
+++ libpgchess.sql
@@ -1,3 +1,23 @@
+-- pgchess.sql : a Chess player in PostgreSQL
+-- Copyright (C) 2010 Gianni Ciolli <gianni.ciolli@2ndQuadrant.it>
+-- 
+-- This program is free software: you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-- Requires: libpg2podg.sql
+
+------------------------------------------------------------
+
 --SET work_mem = 64;
 --SET temp_buffers = 100;
 SET synchronous_commit = off;
